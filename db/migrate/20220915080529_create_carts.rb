e71069e05c62ea366f29fd19525dff1659c34548
shinya524 <gheena1800@gmail.com>
2022-09-15 08:21:39 +0000

    Add model

---
+++ db/migrate/20220915080529_create_carts.rb
@@ -0,0 +1,12 @@
+class CreateCarts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :carts do |t|
+      
+      t.integer :customer_id, null: false
+      t.integer :item_id,     null: false
+      t.integer :quantity,    null: false
+
+      t.timestamps
+    end
+  end
+end
