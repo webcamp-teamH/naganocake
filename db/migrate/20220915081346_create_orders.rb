c436b82b54202480aa6f3942abc1d9edfc9b16ca
yasuhiromikuno <mikuno-0112@outlook.jp>
2022-09-15 09:18:11 +0000

    add model oders admins

---
+++ db/migrate/20220915081346_create_orders.rb
@@ -0,0 +1,16 @@
+class CreateOrders < ActiveRecord::Migration[6.1]
+  def change
+    create_table :orders do |t|
+      t.integer :customer_id,   null: false
+      t.string :postcode,       null: false
+      t.string :adress,         null: false
+      t.string :name,           null: false
+      t.integer :payment_method,null: false
+      t.integer :order_status,  null: false
+      t.integer :shipping_cost, null: false
+      t.integer :grand_total,   null: false
+      
+      t.timestamps
+    end
+  end
+end
