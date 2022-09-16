cfc3b5a8817303f5ff718e2091eaff1b6c684803
yasuhiromikuno <mikuno-0112@outlook.jp>
2022-09-16 04:23:44 +0000

    add model create

--- db/migrate/20220915081346_create_orders.rb
+++ db/migrate/20220915081346_create_orders.rb
@@ -5,11 +5,11 @@ class CreateOrders < ActiveRecord::Migration[6.1]
       t.string :postcode,       null: false
       t.string :adress,         null: false
       t.string :name,           null: false
-      t.integer :payment_method,null: false
-      t.integer :order_status,  null: false
+      t.integer :payment_method,null: false,  default: 0
+      t.integer :order_status,  null: false,  default: 0
       t.integer :shipping_cost, null: false
       t.integer :grand_total,   null: false
-      
+
       t.timestamps
     end
   end
