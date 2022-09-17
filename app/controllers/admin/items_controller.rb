2a49aa56058e6734edce2b1f1d388fcb9708bef4
yasuhiromikuno <mikuno-0112@outlook.jp>
2022-09-17 13:33:47 +0000

    add バリデーション実装/販売ステータス実装未

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -2,6 +2,7 @@ class Admin::ItemsController < ApplicationController
 
   def new
     @item = Item.new
+    @genres = Genre.all
   end
 
   def index
@@ -9,9 +10,12 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
-    item = Item.new(item_params)
-    item.save
-    redirect_to admin_items_path
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_items_path
+    else
+      render :new
+    end
   end
 
   private
