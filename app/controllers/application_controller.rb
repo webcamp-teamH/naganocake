ed0e48dc72b4cea6e7c557e1f726b34e46c707e0
yasuhiromikuno <mikuno-0112@outlook.jp>
2022-09-17 08:24:58 +0000

    add 商品新規登録実装バグ発生中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,3 @@
 class ApplicationController < ActionController::Base
-
-
-  def after_sign_in_path_for(resource)
-    admin_path
-  end
-
-  def after_sign_out_path_for(resource)
-    admin_session_path
-  end
-
-
-
+  before_action :authenticate_admin!
 end
