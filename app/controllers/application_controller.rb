1a73c7aa7d8fb05448631ffea409cc48307b3b1e
kawasemi70 <108577146+kawasemi70@users.noreply.github.com>
2022-09-17 14:58:01 +0000

    add_mypage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,6 @@
 class ApplicationController < ActionController::Base
 
 
-  def after_sign_in_path_for(resource)
-    admin_path
-  end
-
+ 
 
 end
--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,13 @@
 class Public::CustomersController < ApplicationController
+
+
+  def show
+    @customer = current_customer
+  end
+
+  private
+
+  def customers_params
+    params.require(:customer).permit( :last_name,:first_name, :last_name_kana, :first_name_kana, :address, :postcode, :phone_number)
+  end
 end
