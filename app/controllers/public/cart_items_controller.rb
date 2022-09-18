21515e5d45e5d5d6b35f134f2a4c527dbe556972
shinya524 <gheena1800@gmail.com>
2022-09-18 08:15:51 +0000

    update cart

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,8 +1,10 @@
 class Public::CartItemsController < ApplicationController
+  before_action :authenticate_customer!
+
   def index
     @customer = current_customer
-    @cart_items = @customer.cart_items
-    @cart_item = CartItem.new
+    @cart_items = @customer.carts
+    @cart_item = Cart.new
 
     @cart_count = 0
     @cart_items.each do |cart_item|
@@ -14,54 +16,54 @@ class Public::CartItemsController < ApplicationController
 
   def cart_total
     cart_total = 0
-    current_customer.cart_items.each do |cart_item|
+    @customer.carts.each do |cart_item|
       cart_total += cart_item.subtotal
     end
     return cart_total
   end
 
   def create
-    if @cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
-      if @cart_item.present?
-        @cart_item.quantity += params[:cart_item][:quantity].to_i
-        @cart_item.save
-        redirect_to cart_item_path
+    @cart_item_new = Cart.new(cart_params)
+    @cart_item = current_customer.carts.find_by(item_id: params[:cart][:item_id])
+    if Cart.find_by(item_id: params[:cart][:item_id])
+      @cart_item.quantity += params[:cart][:quantity].to_i
+      @cart_item.save
+      redirect_to cart_items_path
+    else
+      @cart_item_new.customer_id = current_customer.id
+      if @cart_item_new.save
+        redirect_to cart_items_path
       else
-        @new_cart_item.customer_id = current_customer.id
-        if @new_cart_item.save
-          redirect_to cart_item_path
-        else
-          redirect_to request.referer
-        end
+        redirect_to request.referer
       end
     end
   end
 
   def update
-    @cart_item = CartItem.find(params[:id])
-    if @cart_item.update(cart_item_params)
-      redirect_to cart_item_path, notice: "変更しました"
+    @cart_item = Cart.find(params[:id])
+    if @cart_item.update(cart_params)
+      redirect_to cart_items_path, notice: "変更しました"
     else
       render :index, notice: "商品数の変更ができませんでした"
     end
   end
 
   def destroy
-    @cart_item = CartItem.find(params[:id])
+    @cart_item = Cart.find(params[:id])
     @cart_item.destroy
     redirect_to cart_items_path
   end
 
   def destroy_all
-    @cart_items = CartItem.all
+    @cart_items = current_customer.carts
     @cart_items.destroy_all
-    render :index
+    redirect_to cart_items_path
   end
 
 
   private
   def cart_item_params
-    params.require(:cart_item).permit(:item_id, :quantity)
+    params.require(:cart).permit(:item_id, :quantity)
   end
 
 end
