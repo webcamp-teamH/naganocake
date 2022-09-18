0ee1ecb9a96a06ce5a56793df08412d6c5d8b104
yasuhiromikuno <mikuno-0112@outlook.jp>
2022-09-18 04:47:34 +0000

    add 商品登録、一覧、詳細画面実装中

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -9,6 +9,14 @@ class Admin::ItemsController < ApplicationController
      @items = Item.all
   end
 
+  def show
+     @item = Item.find(params[:id])
+  end
+
+  def edit
+     @item = Item.find(params[:id])
+  end
+
   def create
     @item = Item.new(item_params)
     if @item.save
