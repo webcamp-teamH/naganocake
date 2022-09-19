aba4fe148417ceddba14737b68e7c96d1b71cf22
kawasemi70 <108577146+kawasemi70@users.noreply.github.com>
2022-09-19 05:59:57 +0000

    mypage

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+   before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
    before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -14,6 +14,8 @@ class Public::RegistrationsController < Devise::RegistrationsController
    def after_sign_out_path_for(resource)
      "/"
    end
+   
+   protected
 
    def configure_permitted_parameters
      devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name,:first_name,:last_name_kana,:first_name_kana,:postcode,:address,:phone_number])
