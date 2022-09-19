0eb58e443211d2955db9fecd32db0fbf3755d4d4
kawasemi70 <108577146+kawasemi70@users.noreply.github.com>
2022-09-19 08:46:54 +0000

    addorder

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,2 +1,27 @@
 class Public::OrdersController < ApplicationController
+  
+  def new
+    @order = Order.new
+  
+  end
+  
+  def confirm
+    
+  end 
+  
+  def finish
+    
+  end 
+  
+  def create
+    
+  end  
+  
+  def index
+    
+  end
+  
+  def show
+    
+  end  
 end
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -3,7 +3,7 @@
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
-  end
+
 
 
 
